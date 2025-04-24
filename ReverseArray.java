8bb8d48cf89d8eea834ce5a778df164f91c5d25c
Anshika <anshikav726@gmail.com>
2025-04-25 00:17:08 +0530

    25-05-2025

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 public class ReverseArray {
     public static void main(String args[]){
-        int arr[]={2,5,7,8,9};
-        reverse(arr);
+        int arr[]={9,5,7,8,7};
+        reverseArr(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void reverse(int[]arr){
+    static void reverseArr(int[]arr){
         int start=0;
         int end=arr.length-1;
         while(start<end){
