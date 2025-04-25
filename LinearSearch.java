9ab8a0b8384ec96f7bdc7451d546f3831e9d2a0a
Anshika <anshikav726@gmail.com>
2025-04-25 23:16:11 +0530

    25-05-2025

--- LinearSearch.java
+++ LinearSearch.java
@@ -14,6 +14,7 @@ public class LinearSearch {
             arr[i] = input.nextInt();
         }
         //Display
+
         System.out.print("You entered:");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
