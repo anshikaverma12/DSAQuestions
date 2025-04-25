54589e44b2319ac6fbf5890f8eb7ed2b8f541058
Anshika <anshikav726@gmail.com>
2025-04-25 23:15:36 +0530

    25-05-2025

---
+++ LinearSearch.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        //length
+        System.out.print("Enter the length of an array:");
+        int length = input.nextInt();
+        //creation
+        int[] arr = new int[length];
+        //elements
+        System.out.print("Enter the elements of an array:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextInt();
+        }
+        //Display
+        System.out.print("You entered:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        //target
+        System.out.print("Enter the target you want to search in array:");
+        int target=input.nextInt();
+        Search(arr,target);
+    }
+    //function for search
+    static int  Search(int[]arr,int target){
+        System.out.println("Your selected target:"+target);
+       for(int i=0;i< arr.length;i++){
+           if(arr[i]==target){
+               System.out.println("Found at index:"+ i);
+               return i;
+           }
+       }
+        return-1;
+    }
+}
